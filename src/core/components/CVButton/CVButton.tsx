064e742267800382a29283b4de766aa04c301671
soufi <nasrin.soufi95@gmail.com>
2025-11-09 10:47:05 +0330

    ch-CV

--- src/core/components/CVButton/CVButton.tsx
+++ src/core/components/CVButton/CVButton.tsx
@@ -5,7 +5,7 @@ import CustomHref from "core/components/CustomHref";
 
 const CVButton: FC<ButtonProps> = (props) => {
   const url =
-    "https://drive.google.com/file/d/1-xWDxYbYdgzjOoriy9EacSvMces_uEqs/view";
+    "https://drive.google.com/file/d/1BSzaklchc9U3wrYOR0JfLBMdQ3ec4-QU/view?usp=drive_link" ;
     
 
   return (
